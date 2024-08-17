5e6863c08793516d7aa0ee58599d6cee7c1c9ba2
Babaushapatil0 <Babushapatil0@gmail.com>
2024-08-17 22:30:16 +0530

    hello first commit from git cmd

--- TestJava/src/com/test/LamdaExpression.java
+++ TestJava/src/com/test/LamdaExpression.java
@@ -1,5 +1,27 @@
 package com.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class LamdaExpression {
+
+	//parameter -> expresssion;
+	//(parameter1, parameter2) -> expression
+	//(parameter1, parameter2) -> { code block }
+	
+	  public static void main(String[] args) {
+		    ArrayList numbers = new ArrayList();
+		    numbers.add(5);
+		    numbers.add(9);
+		    numbers.add(8);
+		    numbers.add(1);
+		    
+		    //numbers.forEach( (n) -> { System.out.println(n); } );
+		    
+		   // numbers.forEach( ( hello ) -> { System.out.println( hello + "hello" ); } );
+		    
+		   
+		  }
 
 }
